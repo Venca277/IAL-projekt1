0f0c082b0ab43de4894e61d56036823a6f25b053
Václav Sovák <venca03@centrum.cz>
2024-10-08 23:46:41 +0200

    added solution to c206-ext, but is still broken

--- c206-ext/c206-ext.c
+++ c206-ext/c206-ext.c
@@ -30,11 +30,53 @@ bool solved;
  * @param packet Ukazatel na strukturu přijatého paketu
  */
 void receive_packet(DLList *packetLists, PacketPtr packet) {
-  int i;
-  for (size_t i = 0; i < MAX_PACKET_COUNT; i++) {
-    if () {
-      /* code */
+  QosPacketListPtr curr_qos;
+  int located = 0;
+
+  DLL_First(packetLists);
+  while (packetLists->activeElement != NULL) {
+    curr_qos = (QosPacketListPtr)packetLists->activeElement->data;
+    if (curr_qos->priority == packet->priority) {
+      located = 1;
+      break;
     }
+    DLL_Next(packetLists);
+  }
+
+  if (located) {
+
+    if (curr_qos->list->currentLength < MAX_PACKET_COUNT) {
+      DLL_InsertLast(curr_qos->list, (long)packet);
+    } else {
+      DLL_First(curr_qos->list);
+      for (int i = 1; i <= curr_qos->list->currentLength; i++) {
+        if (i % 2) {
+          if (curr_qos->list->activeElement->nextElement != NULL) {
+            DLL_Next(curr_qos->list);
+          }
+        } else if (curr_qos->list->activeElement->nextElement != NULL) {
+          DLL_Next(curr_qos->list);
+          DLL_DeleteBefore(curr_qos->list);
+        }
+      }
+    }
+
+  } else {
+    DLList *new_list;
+    QosPacketList *new_packet_list;
+    new_list = malloc(sizeof(DLList));
+    new_packet_list = malloc(sizeof(QosPacketList));
+    if (new_list == NULL || new_packet_list == NULL) {
+      printf("Allocation failed!");
+      return;
+    }
+
+    new_packet_list->priority = packet->priority;
+    new_packet_list->list = new_list;
+    DLL_InsertLast(packetLists, (long)new_packet_list);
+
+    curr_qos = (QosPacketListPtr)packetLists->lastElement->data;
+    DLL_InsertLast(curr_qos->list, (long)packet);
   }
 }
 
@@ -57,5 +99,72 @@ void receive_packet(DLList *packetLists, PacketPtr packet) {
  */
 void send_packets(DLList *packetLists, DLList *outputPacketList,
                   int maxPacketCount) {
-  solved = false; /* V případě řešení, smažte tento řádek! */
+  int swapp = 1;
+  long swapp_place;
+  QosPacketListPtr active;
+  QosPacketListPtr active_next;
+  DLL_First(packetLists);
+
+  while (swapp) {
+    swapp = 0;
+    int i = 1;
+
+    while (i <= packetLists->currentLength) {
+
+      if (packetLists->activeElement->nextElement == NULL) {
+        DLL_First(packetLists);
+      }
+      active = (QosPacketListPtr)packetLists->activeElement->data;
+      active_next =
+          (QosPacketListPtr)packetLists->activeElement->nextElement->data;
+      if ((active == NULL) || (active_next == NULL)) {
+        break;
+      }
+
+      if (active->priority < active_next->priority) {
+        swapp_place = packetLists->activeElement->nextElement->data;
+        packetLists->activeElement->nextElement->data =
+            packetLists->activeElement->data;
+        packetLists->activeElement->data = swapp_place;
+        swapp = 1;
+      }
+
+      DLL_Next(packetLists);
+      i++;
+    }
+  }
+
+  DLL_First(packetLists);
+  QosPacketListPtr curr_qos;
+  curr_qos = (QosPacketListPtr)packetLists->activeElement->data;
+  DLL_First(curr_qos->list);
+  int packets_out = 0;
+
+  while (packets_out != maxPacketCount) {
+    if (curr_qos->list->activeElement == NULL) {
+      DLL_Next(packetLists);
+      if (packetLists->activeElement == NULL) {
+        break;
+      }
+      curr_qos = (QosPacketListPtr)packetLists->activeElement->data;
+    }
+
+    if (curr_qos->list->currentLength == 1) {
+      DLL_InsertLast(outputPacketList, curr_qos->list->activeElement->data);
+      packets_out++;
+      DLL_DeleteFirst(curr_qos->list);
+      continue;
+    }
+
+    DLL_InsertLast(outputPacketList, curr_qos->list->activeElement->data);
+    packets_out++;
+    DLL_Next(curr_qos->list);
+    DLL_DeleteBefore(curr_qos->list);
+    if ((curr_qos->list->activeElement == curr_qos->list->firstElement) &&
+        (curr_qos->list->currentLength == 1)) {
+      DLL_InsertLast(outputPacketList, curr_qos->list->activeElement->data);
+      packets_out++;
+      DLL_DeleteFirst(curr_qos->list);
+    }
+  }
 }
